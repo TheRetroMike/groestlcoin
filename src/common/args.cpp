6f7242a9c86993f4cb839d07f2b438442093b0dc
gruve-p <jackielove4u@hotmail.com>
2023-04-21 16:13:46 +0200

    Rename config file name

--- src/common/args.cpp
+++ src/common/args.cpp
@@ -34,7 +34,7 @@
 #include <string>
 #include <utility>
 
-const char * const BITCOIN_CONF_FILENAME = "bitcoin.conf";
+const char * const BITCOIN_CONF_FILENAME = "groestlcoin.conf";
 const char * const BITCOIN_SETTINGS_FILENAME = "settings.json";
 
 ArgsManager gArgs;
@@ -189,7 +189,7 @@ bool ArgsManager::ParseParameters(int argc, const char* const argv[], std::strin
         if (key.substr(0, 5) == "-psn_") continue;
 #endif
 
-        if (key == "-") break; //bitcoin-tx using stdin
+        if (key == "-") break; //groestlcoin-tx using stdin
         std::optional<std::string> val;
         size_t is_index = key.find('=');
         if (is_index != std::string::npos) {
@@ -683,12 +683,12 @@ std::string HelpMessageOpt(const std::string &option, const std::string &message
 
 fs::path GetDefaultDataDir()
 {
-    // Windows: C:\Users\Username\AppData\Roaming\Bitcoin
-    // macOS: ~/Library/Application Support/Bitcoin
-    // Unix-like: ~/.bitcoin
+    // Windows: C:\Users\Username\AppData\Roaming\Groestlcoin
+    // macOS: ~/Library/Application Support/Groestlcoin
+    // Unix-like: ~/.groestlcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Groestlcoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -698,10 +698,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // macOS
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/Groestlcoin";
 #else
     // Unix-like
-    return pathRet / ".bitcoin";
+    return pathRet / ".groestlcoin";
 #endif
 #endif
 }
